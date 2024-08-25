33adb0283684950676259fb9a606e8df9d036386
hitenxsingh22 <hitensingh2212@gmail.com>
2024-08-25 12:48:28 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+//navbar component
+
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
